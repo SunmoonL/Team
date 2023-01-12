790c24b5cdbbe4510bc3f021e3d7a1852853c645
SunmoonL <dltjsans123@naver.com>
2023-01-12 18:30:11 +0900

    23.01.12

---
+++ Team/js/index.js
@@ -0,0 +1,16 @@
+// $(function(){
+//     $('nav').mouseenter(function(){
+//         $('nav > ul > li > div').stop().slideDown();
+//         $('.small > nav > ul > li > a').css('color','black');
+//         $('.small > i').css('color','black');
+//         $('.navdown').css('height','300px');
+//     });
+
+//      $('nav').mouseleave(function(){
+//         $('.navlist > div > ul > li > div > a').css('color','#fff');
+//         $('.small > nav > ul > li > a').css('color','#fff');
+//         $('.small > i').css('color','#fff');
+//         $('.navdown').css('height','0');
+//         $('nav > ul > li > div').stop().slideUp("fast");
+//     });
+// });
