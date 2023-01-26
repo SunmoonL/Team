3eacb4ca34f5f9ee5cabfaa87fdd1ca36d603b4c
SunmoonL <dltjsans123@naver.com>
2023-01-26 17:54:55 +0900

    23.01.26

--- Team/js/index.js
+++ Team/js/index.js
@@ -71,7 +71,7 @@ $(function(){
         $($(this)).addClass("on");
         $($(this)).siblings().removeClass("on");
     })
-})
+});
 
 $(function(){
     $("#pager > a").eq(1).click(function(){
@@ -80,7 +80,7 @@ $(function(){
         $($(this)).addClass("on");
         $($(this)).siblings().removeClass("on");
     })
-})
+});
 
 $(function(){
     $("#subtab > li").eq(0).click(function(){
@@ -96,7 +96,7 @@ $(function(){
         });
         $("#pager > a").eq(1).show().removeClass("on");
     })
-})
+});
 
 $(function(){
     $("#subtab > li").eq(1).click(function(){
@@ -108,7 +108,7 @@ $(function(){
         $("#pager > a").eq(0).off("click").click();
         $("#pager > a").eq(1).hide();
     })
-})
+});
 
 $(function(){
     $("#subtab > li").eq(2).click(function(){
@@ -120,7 +120,7 @@ $(function(){
         $("#pager > a").eq(0).off("click").click();
         $("#pager > a").eq(1).hide();
     })
-})
+});
 
 $(function(){
     $("#subtab > li").eq(3).click(function(){
@@ -132,7 +132,61 @@ $(function(){
         $("#pager > a").eq(0).off("click").click();
         $("#pager > a").eq(1).hide();
     })
-})
+});
+
+$(function(){
+    $("#mobmenu > ul > li:nth-of-type(2)").click(function(){
+        $("#secondmenu").toggle();
+    });
+});
+
+$(document).on('change', "#select", function () {
+    if ($(this).val() == 1) {
+        $("#list1").show();
+        $("#list2,#list3,#list4,#list5").hide();
+        $($(this)).addClass("active1");
+        $($(this)).siblings().removeClass("active1");
+        $("#pager > a").eq(0).on("click", function () {
+            $("#list1").show();
+            $("#list2,#list3,#list4,#list5").hide();
+            $($(this)).addClass("on");
+            $($(this)).siblings().removeClass("on");
+        });
+        $("#pager > a").eq(1).show().removeClass("on");
+    } else if ($(this).val() == 2) {
+        $("#list3").show();
+        $("#list1,#list2,#list4,#list5").hide();
+        $($(this)).addClass("active1");
+        $($(this)).siblings().removeClass("active1");
+        $("#pager > a").eq(0).addClass("on");
+        $("#pager > a").eq(0).off("click").click();
+        $("#pager > a").eq(1).hide();
+    } else if ($(this).val() == 3) {
+        $("#list4").show();
+        $("#list2,#list1,#list3,#list5").hide();
+        $($(this)).addClass("active1");
+        $($(this)).siblings().removeClass("active1");
+        $("#pager > a").eq(0).addClass("on");
+        $("#pager > a").eq(0).off("click").click();
+        $("#pager > a").eq(1).hide();
+    } else if ($(this).val() == 4) {
+        $("#list5").show();
+        $("#list2,#list3,#list4,#list1").hide();
+        $($(this)).addClass("active1");
+        $($(this)).siblings().removeClass("active1");
+        $("#pager > a").eq(0).addClass("on");
+        $("#pager > a").eq(0).off("click").click();
+        $("#pager > a").eq(1).hide();
+    }
+});
+
+$(function(){
+    $("#write > div:nth-of-type(2) > a").click(function(){
+        $($(this)).siblings().removeClass("active3");
+        $($(this)).addClass("active3");
+    })
+});
+
 //footer
 $(document).ready(function(){
     $('.business_btn').click(function(){
